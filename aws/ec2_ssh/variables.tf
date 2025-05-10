82139349418a840ac7b342acf449e9c78156e674
elfedy <federico@moonsonglabs.com>
2025-05-10 14:04:26 -0300

    no default ami

--- aws/ec2_ssh/variables.tf
+++ aws/ec2_ssh/variables.tf
@@ -18,5 +18,4 @@ variable "ssh_public_key" {
 variable "ami_id" {
   description = "AMI ID for the EC2 instance"
   type        = string
-  default     = "ami-0735c191cf914754d" # Ubuntu 22.04 LTS in us-west-2
 } 
